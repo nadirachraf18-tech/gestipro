3eb1e684d7f2ea046f5a000a010759aa800480cc
nadirachraf18-tech <nadirachraf18@gmail.com>
2026-09-24 16:24:37 +0100

    fix: use hash router for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,2 @@
-import React from 'react';import{createRoot}from'react-dom/client';import{BrowserRouter}from'react-router-dom';import App from './App';import './styles.css';
-createRoot(document.getElementById('root')).render(<React.StrictMode><BrowserRouter><App/></BrowserRouter></React.StrictMode>);
+import React from 'react';import{createRoot}from'react-dom/client';import{HashRouter}from'react-router-dom';import App from './App';import './styles.css';
+createRoot(document.getElementById('root')).render(<React.StrictMode><HashRouter><App/></HashRouter></React.StrictMode>);
